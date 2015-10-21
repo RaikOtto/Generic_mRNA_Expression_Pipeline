2d79117e7a84dc67bfe8c8b26866741f7d9e50cb
RaikOtto <fubioinf@gmail.com>
2015-10-21 11:58:36 +0200

    update

--- Main_script.r
+++ Main_script.r
@@ -30,17 +30,14 @@ source( "Src/pipeline_structure.r" )
 create_cohorts    = T # 2
 parse_files       = T # 3
 normalize         = T # 4
-<<<<<<< HEAD
 qc_control        = T # 5
 annotate          = T # 6
 absent_analysis   = T # 7
 dif_exp_ana       = T # 8
-=======
 qc_control        = F # 5
 annotate          = F # 6
 absent_analysis   = F # 7
 dif_exp_ana       = F # 8
->>>>>>> 9014c1e20cbd2965055c78d99b98d0baae6e8764
 export_results    = F # 9
 create_pathways   = F # 10
 extract_interest  = F # 11
