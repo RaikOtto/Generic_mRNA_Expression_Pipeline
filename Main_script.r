c03d599d30605b3759241710d2435f7bd739d97d
root <root@kocent.local>
2015-10-26 17:04:03 +0100

    update

--- Main_script.r
+++ Main_script.r
@@ -19,10 +19,11 @@ print(c("Kocent:",kocent ))
 
 setwd( pipeline_loc ) # Set the path to where the pipeline is located
 
+#options(error=traceback)
 default_parameters = T
 which_project = "immuMZ"
 source("project_files.r")
-
+options(error=traceback)
 source( "Src/pipeline_structure.r" )
 
 ###
@@ -32,15 +33,8 @@ parse_files       = T # 3
 normalize         = T # 4
 qc_control        = T # 5
 annotate          = T # 6
-absent_analysis   = T # 7
-dif_exp_ana       = T # 8
-<<<<<<< HEAD
-qc_control        = F # 5
-annotate          = F # 6
 absent_analysis   = F # 7
-dif_exp_ana       = F # 8
-=======
->>>>>>> d9c53926e8aac703f3a6e2299e85a066d3e0d0f7
+dif_exp_ana       = F2 # 8
 export_results    = F # 9
 create_pathways   = F # 10
 extract_interest  = F # 11
