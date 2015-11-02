6b1fca3a9556fcda689538af7526e3fbfb17b62c
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2015-11-02 11:37:51 +0100

    update

--- Main_script.r
+++ Main_script.r
@@ -36,12 +36,12 @@ parse_files       = T # 3
 normalize         = T # 4
 qc_control        = F # 5
 annotate          = T # 6
-absent_analysis   = F # 7
+absent_analysis   = T # 7
 dif_exp_ana       = T # 8
-export_results    = F # 9
-create_pathways   = F # 10
-extract_interest  = F # 11
-annotate_tissue_abbundance = F # 12
+export_results    = T # 9
+create_pathways   = T # 10
+extract_interest  = T # 11
+annotate_tissue_abbundance = T # 12
 
 ### generic
 stat_design = "contrast"
