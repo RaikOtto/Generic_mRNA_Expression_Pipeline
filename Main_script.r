f3a96e8dd3219f3efb0cf6f2f9ce3ff2d3cdf6dc
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2015-11-05 16:01:17 +0100

    update

--- Main_script.r
+++ Main_script.r
@@ -24,7 +24,7 @@ setwd( pipeline_loc ) # Set the path to where the pipeline is located
 #options(error=traceback)
 default_parameters = T
 
-which_project = "MZ_no_sici"
+which_project = "immuMZ"
 
 source("project_files.r")
 options(error=traceback)
