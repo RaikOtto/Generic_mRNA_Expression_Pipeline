2cbb3a3b8ecbad3d1bae235da115331bc17d3950
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2015-12-14 17:46:56 +0100

    update

--- Main_script.r
+++ Main_script.r
@@ -44,9 +44,9 @@ qc_control        = F # 5
 annotate          = T # 6
 absent_analysis   = F # 7
 dif_exp_ana       = T # 8
-export_results    = F # 9
-create_pathways   = F # 10
-extract_interest  = F # 11
+export_results    = T # 9
+create_pathways   = T # 10
+extract_interest  = T # 11
 create_heatmaps_genes_of_interest = F # 12
 annotate_tissue_abbundance = F # 13
 
