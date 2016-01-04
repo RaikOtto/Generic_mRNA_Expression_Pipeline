d1776cab6c68bc8137a53bbcbd5a052d62b04ebb
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2016-01-04 11:18:27 +0100

    update

--- Main_script.r
+++ Main_script.r
@@ -24,7 +24,7 @@ setwd( pipeline_loc ) # Set the path to where the pipeline is located
 #options(error=traceback)
 default_parameters = T
 
-which_project = "GSE43795"
+which_project = "GSE14407"
 
 source("project_files.r")
 options( error = traceback )
@@ -40,13 +40,13 @@ use_gsea   = F
 create_cohorts    = T # 2
 parse_files       = T # 3
 normalize         = F # 4
-qc_control        = F # 5
-annotate          = T # 6
+qc_control        = T # 5
+annotate          = F # 6
 absent_analysis   = F # 7
-dif_exp_ana       = T # 8
-export_results    = T # 9
-create_pathways   = T # 10
-extract_interest  = T # 11
+dif_exp_ana       = F # 8
+export_results    = F # 9
+create_pathways   = F # 10
+extract_interest  = F # 11
 create_heatmaps_genes_of_interest = F # 12
 annotate_tissue_abbundance = F # 13
 
