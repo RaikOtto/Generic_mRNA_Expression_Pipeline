179239459e3c2575c54abf4adfdd2ac5e47affe3
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2016-02-11 13:36:19 +0100

    update

--- Main_script.r
+++ Main_script.r
@@ -24,7 +24,7 @@ setwd( pipeline_loc ) # Set the path to where the pipeline is located
 #options(error=traceback)
 default_parameters = T
 
-which_project = "ovarian"
+which_project = "HNSC"
 
 source("project_files.r")
 options( error = traceback )
@@ -33,17 +33,17 @@ source( "Src/pipeline_structure.r" )
 ### set run parameters
 
 var_filter = F
-use_gsea   = T
+use_gsea   = F
 
 #p_val = 0.05
 
 create_cohorts    = T # 2
 parse_files       = T # 3
-normalize         = F # 4
+normalize         = T # 4
 qc_control        = F # 5
-annotate          = F # 6
+annotate          = T # 6
 absent_analysis   = F # 7
-dif_exp_ana       = F # 8
+dif_exp_ana       = T # 8
 export_results    = F # 9
 create_pathways   = F # 10
 extract_interest  = F # 11
