048e6005c977ab3737a8fe120d9dd2c60b2f80de
RaikOtto <fubioinf@gmail.com>
2016-02-15 11:52:46 +0100

    update

--- Main_script.r
+++ Main_script.r
@@ -37,17 +37,17 @@ use_gsea   = F
 
 #p_val = 0.05
 
-create_cohorts    = F # 2
-parse_files       = F # 3
-normalize         = F # 4
+create_cohorts    = T # 2
+parse_files       = T # 3
+normalize         = T # 4
 qc_control        = F # 5
 annotate          = T # 6
 absent_analysis   = F # 7
-dif_exp_ana       = F # 8
-export_results    = F # 9
+dif_exp_ana       = T # 8
+export_results    = T # 9
 create_pathways   = F # 10
-extract_interest  = F # 11
-create_heatmaps_genes_of_interest = F # 12
+extract_interest  = T # 11
+create_heatmaps_genes_of_interest = T # 12
 annotate_tissue_abbundance = F # 13
 
 ### generic
