585b5ee7fbba16a5c3d11afbb18cbcb2f17a2f6e
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2016-02-24 16:33:39 +0100

    update

--- Src/test_packages.R
+++ Src/test_packages.R
@@ -1,8 +1,6 @@
 pack_bioc = c( "limma", "KEGG.db", "pathview", "stringr", "hgu133plus2.db", "affy", "simpleaffy", "affyPLM", "affycoretools", "affyQCReport", "annaffy", "hgu133a.db", "oligoData", "arrayQualityMetrics", "genefilter", "oligo", "pd.huex.1.0.st.v2", "lumi", "GEOquery", "illuminaHumanv4.db" )
 pack_cran = c( "xlsx", "gdata", "RColorBrewer", "stringr", "WriteXLS", "plotly" )
 
-check_bioc = pack_bioc %in% installed.packages()
-ckeck_cran = pack_cran %in% installed.packages()
 index_bioc = which( ! pack_bioc %in% installed.packages() )
 index_cran = which( ! pack_cran %in% installed.packages() )
 
