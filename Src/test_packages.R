7e134847caf3e37efe7ce9684fc8732fd6189e7b
Jan-Niklas Rössler <jan-niklas.roessler@fu-berlin.de>
2016-02-24 16:22:24 +0100

    update

--- Src/test_packages.R
+++ Src/test_packages.R
@@ -1,2 +1,22 @@
-packages = c( "xlsx", "gdata", "RColorBrewer", "limma", "KEGG.db", "pathview", "stringr", "hgu133plus2.db", "affy", "simpleaffy", "affyPLM", "affycoretools", "affyQCReport", "annaffy", "hgu133a.db", "oligoData", "arrayQualityMetrics", "WriteXLS", "genefilter", "plotly", "oligo", "pd.huex.1.0.st.v2", "lumi", "GEOquery", "illuminaHumanv4.db" )
+pack_bioc = c( "limma", "KEGG.db", "pathview", "stringr", "hgu133plus2.db", "affy", "simpleaffy", "affyPLM", "affycoretools", "affyQCReport", "annaffy", "hgu133a.db", "oligoData", "arrayQualityMetrics", "genefilter", "oligo", "pd.huex.1.0.st.v2", "lumi", "GEOquery", "illuminaHumanv4.db" )
+pack_cran = c( "xlsx", "gdata", "RColorBrewer", "stringr", "WriteXLS", "plotly" )
+
+check_bioc = pack_bioc %in% installed.packages()
+ckeck_cran = pack_cran %in% installed.packages()
+index_bioc = which( ! pack_bioc %in% installed.packages() )
+index_cran = which( ! pack_cran %in% installed.packages() )
+
+if ( length( index_bioc ) > 0){
+  current = pack_bioc[index_bioc]
+  source("https://bioconductor.org/biocLite.R")
+  biocLite(current)
+}
+
+if ( length( index_cran ) > 0){
+  current = pack_cran[index_cran]
+  install.packages(current)
+}
+
+packages = c(pack_bioc, pack_cran)
+
 print( suppressMessages( sapply( packages, require, character.only=TRUE, quietly = TRUE ) ) )
